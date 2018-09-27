88baf2d00991cf44984d8cb7816a43bf3a7c783d
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-27 21:48:35 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+class Stack {
+	private String[] stack;
+	private int size, resize;
+	public Stack() {
+		stack = new String[20];
+		size = 0;
+		resize = 40;
+	}
+	public void push(String item) {
+		if (size == resize) {
+			resize(2 * size);
+		}
+		stack[size++] = item;
+	}
+	public String pop() {
+		if (size == resize / 4) {
+			resize(size);
+		}
+		size--;
+		return stack[size];
+
+	}
+	public void resize(int resize) {
+		String[] stack1 = new String[resize];
+		for (int i = 0; i < size; i++) {
+			stack1[i] = stack[i];
+		}
+		stack = stack1;
+	}
+	public boolean isEmpty() {
+		return (size == 0);
+	}
+	public void printstack() {
+		for (int i = 0; i < size; i++) {
+			System.out.println(stack[i] + " ");
+		}
+	}
+	public boolean balanced(String[] paren) {
+		String p;
+		for (int i = 0; i < paren.length; i++) {
+			p = paren[i];
+			if (p.equals("(") || p.equals("[") || p.equals("{")) {
+	 
+				push(p);
+			}
+			if (p.equals(")") || p.equals("]") || p.equals("}")) {
+				if (!p.equals(pop()) || size == 0) {
+					return false;
+				}
+			}
+		}
+		if (size == 0)
+			return true;
+		return false;
+	}
+	
+}
+
+public class Solution {
+	public static String[] convert(String s) {
+		String[] s1 = new String[s.length()];
+		for (int i =0; i < s.length(); i++) {
+			String s3 = s.charAt(i) + " ";
+			s1[i] = s3.substring(0, 1);
+		}
+		return s1;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		String s;
+		for (int i = 0; i < n; i++) {
+			s = sc.nextLine();
+			Stack pl = new Stack();
+			String[] s2 = convert(s);
+			if (pl.balanced(s2)) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+			
+		}
+
+
+
+	}
+
+}
