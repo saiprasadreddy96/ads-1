2bd346f9dce737c3fb0c1a04dad43216a454a78e
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-09-28 18:32:29 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+public class Solution {
+	public void find(int np, int pos) {
+		int[] persons = new int[np];
+		for (int i = 0; i < np; i++) {
+			persons[i] = i;
+		}
+		int count = np, i = 1, index = 0;
+		while (count != 0) {
+			
+			if (persons[index % np] != -999) {
+				if (i == pos) {
+					System.out.println(persons[index % np]);
+					persons[index % np] = -999;
+					count--;
+					i = 1;
+					index++;
+				} else {
+					index++;
+					i++;
+				}
+			} else {
+				index++;
+			}
+
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Solution s = new Solution();
+		int n = sc.nextInt();
+		int np, p;
+		for (int i = 0; i < n; i++) {
+			np = sc.nextInt();
+			p = sc.nextInt();
+			//System.out.println(np,p);
+			s.find(np, p);
+
+		}
+	}
+}
