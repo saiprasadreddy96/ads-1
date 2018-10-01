a20e05fe8319c6036674f9aec39dfffa66951e4d
saiprasadreddy96 <mspreddy1996@msitprogram.net>
2018-10-01 11:54:46 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ m7/7.1 Steque/Solution.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+class LinkedList<E> {
+	private Node head, tail;
+	class Node {
+		private E data;
+		private Node next;
+		public Node(E item) {
+			this.data = item;
+			this.next = null;
+		}
+	}
+	public LinkedList() {
+		head = null;
+		tail = null;
+	}
+	public void push(E item) {
+		Node newnode = new Node(item);
+		if (head == null) {
+			tail = newnode;
+		}
+		newnode.next = head;
+		head = newnode;
+	}
+	public void pop() {
+		if (head == null) {
+			System.out.println("Solution is empty.");
+			return;
+		}
+		head = head.next;
+		if (head == null) 
+			tail = head;
+	}
+	public void enqueue(E item) {
+		if (head == null) {
+			push(item);
+			return;
+		}
+		Node newnode = new Node(item);
+		tail = newnode;
+		newnode.next = null;
+	}
+	public void print() {
+		Node temp = head;
+		String str = "";
+		while(temp.next != null) {
+			str += (temp.data) + ", ";
+		}
+		System.out.println(str.trim());
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			LinkedList<Integer> ll = new LinkedList<Integer>();
+			while(sc.hasNext()) {
+				String[] str = sc.nextLine().split(" ");
+				switch (str[0]) {
+					case "push":
+						ll.push(Integer.parseInt(str[1]));
+						break;
+					case "pop":
+						ll.pop();
+						break;
+					case "enqueue":
+						ll.enqueue(Integer.parseInt(str[1]));
+						break;
+					default:
+						break;
+				}
+				ll.print();
+			}
+		}
+		
+	}
+}
